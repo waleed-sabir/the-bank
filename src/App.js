b6adc06c1c6608e412aa80adbe0addc7fc63fe24
waleed-sabir <waleed_sabir@yahoo.com>
2022-02-28 13:31:57 +0500

    added page routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+// page components
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
