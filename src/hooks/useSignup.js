4c2dc5507a82487ae3daf35401d9130a0c3eb023
waleed-sabir <waleed_sabir@yahoo.com>
2022-02-28 16:47:31 +0500

    created useAuthContext hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { auth } from "../firebase/config";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
 
   const signup = async (email, password, displayName) => {
     setError(null);
@@ -13,7 +15,7 @@ export const useSignup = () => {
     try {
       // signup
       const res = await createUserWithEmailAndPassword(auth, email, password);
-      console.log(res.user);
+      // console.log(res.user);
 
       if (!res) {
         throw new Error("Could not complete the signup");
@@ -21,6 +23,9 @@ export const useSignup = () => {
 
       // add displayName to user
       await updateProfile(res.user, { displayName });
+
+      // dispatch a LOGIN action
+      dispatch({ type: "LOGIN", payload: res.user });
 
       setIsPending(false);
       setError(null);
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <AuthContextProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
